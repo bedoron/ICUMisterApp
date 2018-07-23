91b88dd01182081f6ccadcd48b4133161707a5ce
Oleksandr Iezhov <oiezhov@gmail.com>
2018-07-23 17:11:44 +0300

    Basic notification service added

---
+++ app/src/main/java/com/icumister/notification/NotificationSettings.java
@@ -0,0 +1,8 @@
+package com.icumister.notification;
+
+public class NotificationSettings {
+
+    public static String SenderId = "900238843160";
+    public static String HubName = "ICUMisterNotificationHub";
+    public static String HubListenConnectionString = "Endpoint=sb://icumisterhub.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=kdYhzJavhEMFLBfidk03uHInE8WpuPAjUuR78UX/S4I=";
+}
